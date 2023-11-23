256fc92b66467d52327cf13548c1dfe8ad403eda
chomchom96 <cyy8071@gmail.com>
2023-11-23 20:23:14 +0900

    1123 parrot

--- FirstProject-Front/src/components/common/about.vue
+++ FirstProject-Front/src/components/common/about.vue
@@ -1,48 +1,50 @@
 <template>
-    <main class="content">
-      <div class="row m-8 p-3 mt-2" style="text-align:center; justify-content:space-between;">
-        <div class="card col p-5 border-light page-preview shadow-sm cust_box  page-preview scale-up-2">
-          <div class="card-body pb-3">
-            <img src="@/assets/cat.png" class="user-avatar scale-up-2 mt-5 large-avatar  mx-auto mb-4" alt="User Avatar" style="width: 200px;">
-            <h4 class="h2 fw-bold">🧑 조용운</h4> <br>
-            <h5 class="fw-normal">10기 서울 5반</h5>
-            <br><br>
-            <div style="text-align:left; margin-left:20px;">
-              <p class="text-gray mt-5 mb-0  page-preview scale-up-2">
-                <i class="bi bi-mailbox"></i> cyy8071@naver.com
-              </p>
-              <p class="text-gray mt-0 mb-0  page-preview scale-up-2">
-                <i class="bi bi-github"></i><a href="https://github.com/chomchom96"> chomchom96</a>
-              </p>
-              <p class="text-gray mt-0 mb-0  page-preview scale-up-2">
-                <i class="bi bi-phone"></i>  010-5040-8071
-              </p>
-            </div>
-          </div>
-        </div>
-  
-        <div class="card col p-5 border-light page-preview shadow-sm cust_box  page-preview scale-up-2">
-          <div class="card-body pb-3">
-            <img src="@/assets/coder.png" class="user-avatar scale-up-2 mt-5 large-avatar mx-auto mb-4" alt="User Avatar" style="width: 200px;">
-            <h4 class="h2 fw-bold">👍 백천주</h4> <br>
-            <h5 class="fw-normal">10기 서울 5반</h5>
-            <br><br>
-            <div style="text-align:left; margin-left:20px;">
-              <p class="text-gray mt-5 mb-0  page-preview scale-up-2">
-                <i class="bi bi-mailbox"></i> kakapokachip@gmail.com
-              </p>
-              <p class="text-gray mt-0 mb-0  page-preview scale-up-2">
-                <i class="bi bi-github"></i> <a href="https://github.com/lookoblong"> lookoblong</a>
-              </p>
-              <p class="text-gray mt-0 mb-0  page-preview scale-up-2">
-                <i class="bi bi-phone"></i> 010-9918-8441
-              </p>
-            </div>
+  <main class="content">
+    <!-- <div class="sub-container"> -->
+    <div class="row m-8 p-3 mt-2" style="text-align:center; width:100%">
+      <div class="card col p-5 border-light page-preview shadow-sm cust_box  page-preview scale-up-2" >
+        <div class="card-body pb-3">
+          <img src="@/assets/cat.png" class="user-avatar scale-up-2 mt-5 large-avatar  mx-auto mb-4" alt="User Avatar" style="width: 150px;">
+          <h4 class="h2 fw-bold">🧑 조용운</h4> <br>
+          <h5 class="fw-normal">SSAFY 10기</h5>
+          <h5 class="fw-normal">서울 5반</h5>
+          <div style="text-align:left; margin-left: 100px;">
+            <p class="text-gray mt-5 mb-0  page-preview scale-up-2">
+              <i class="bi bi-mailbox"></i> cyy8071@naver.com
+            </p>
+            <p class="text-gray mt-0 mb-0  page-preview scale-up-2">
+              <i class="bi bi-github"></i><a href="https://github.com/chomchom96"> chomchom96</a>
+            </p>
+            <p class="text-gray mt-0 mb-0  page-preview scale-up-2">
+              <i class="bi bi-phone"></i>  010-5040-8071
+            </p>
           </div>
         </div>
       </div>
-    </main>
-  </template>
+
+      <div class="card col p-5 border-light page-preview shadow-sm cust_box  page-preview scale-up-2">
+        <div class="card-body pb-3">
+          <img src="@/assets/vueparrot.gif" class="user-avatar scale-up-2 mt-5 large-avatar mx-auto mb-4" alt="User Avatar" style="width: 150px;">
+          <h4 class="h2 fw-bold">🤗 백천주</h4> <br>
+          <h5 class="fw-normal">SSAFY 10기</h5>
+          <h5 class="fw-normal">서울 5반</h5>
+          <div style="text-align:left; margin-left: 100px;">
+            <p class="text-gray mt-5 mb-0  page-preview scale-up-2">
+              <i class="bi bi-mailbox"></i> kakapokachip@gmail.com
+            </p>
+            <p class="text-gray mt-0 mb-0  page-preview scale-up-2">
+              <i class="bi bi-github"></i> <a href="https://github.com/lookoblong"> lookoblong</a>
+            </p>
+            <p class="text-gray mt-0 mb-0  page-preview scale-up-2">
+              <i class="bi bi-phone"></i> 010-9918-8441
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- </div> -->
+  </main>
+</template>
 <script setup>
 
 </script>
@@ -50,12 +52,24 @@
 
 <style scoped>
 p {
-    font-size: 20px;
-    padding: 7px;
+  font-size: 20px;
+  padding: 7px;
+}
+
+.p5 {
+  padding: 0; 
 }
 
 .card {
-    padding : 50px;
-    border: 2px solid lightgrey;
+  padding: 50px;
+  margin: 200px;
+  border: 2px solid lightgrey;
+}
+
+.sub-container {
+  /* max-width: 1200px; */
+  /* margin: 0 auto; */
+  justify-content: center;
+
 }
 </style>
